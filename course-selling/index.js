eba469f53d67dc788b5a744615486901ac256fca
RISHABRAJ VERMA <113842786+rishabrajverma44@users.noreply.github.com>
2023-11-25 01:45:16 +0530

    Update index.js

--- course-selling/index.js
+++ course-selling/index.js
@@ -10,7 +10,7 @@ const PORT=process.env.PORT;
 const app = express();
 app.use(cors(
     {
-        origin: ["https://deploy-mern-frontend.vercel.app"],
+        origin: ["https://course-selling-app-six.vercel.app/"],
         methods: ["POST", "GET"],
         credentials: true
     }
