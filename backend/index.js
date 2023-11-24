1f2e55e58feddb43ac614542e5df3584922d5e9a
RISHABRAJ VERMA <113842786+rishabrajverma44@users.noreply.github.com>
2023-11-24 22:56:26 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const PORT=process.env.PORT;
 const app = express();
 app.use(cors(
     {
-        origin: ["https://deploy-mern-frontend.vercel.app"],
+        origin: [""],
         methods: ["POST", "GET"],
         credentials: true
     }
