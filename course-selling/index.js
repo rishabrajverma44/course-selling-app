462b0c01779df9e44ef1c7e552cce542810b0115
RISHABRAJ VERMA <113842786+rishabrajverma44@users.noreply.github.com>
2024-01-16 14:34:37 +0530

    Update index.js

--- course-selling/index.js
+++ course-selling/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-require('dotenv').config();
+require('dotenv').config({ path: "config/config.env" });
 const mongoose = require("mongoose");
 const cors = require("cors");
 const userController = require("./controller/user");
