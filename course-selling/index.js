c9c161c26743c777553d45021836eca7972d0698
RISHABRAJ VERMA <113842786+rishabrajverma44@users.noreply.github.com>
2024-01-16 13:10:28 +0530

    Update index.js

--- course-selling/index.js
+++ course-selling/index.js
@@ -17,12 +17,8 @@ const app = express();
 //     }
 // ));
 // app.options('*', cors());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+
+
 
 app.use(express.json());
 // Set the view engine to EJS
