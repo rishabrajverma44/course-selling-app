5c4169710903afdbfd9670f6f94ae5f039435db9
RISHABRAJ VERMA <113842786+rishabrajverma44@users.noreply.github.com>
2024-01-16 13:44:24 +0530

    Update index.js

--- course-selling/index.js
+++ course-selling/index.js
@@ -17,6 +17,26 @@ const app = express();
 //     }
 // ));
 // app.options('*', cors());
+// CORS middleware wrapper
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://react-e-commerce-kappa.vercel.app"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(204).end();
+    return;
+  }
+  return await fn(req, res);
+};
 
 
 
