825221df9d94363233a86984147aa4704f7d1906
RISHABRAJ VERMA <113842786+rishabrajverma44@users.noreply.github.com>
2024-01-16 13:02:00 +0530

    Update index.js

--- course-selling/index.js
+++ course-selling/index.js
@@ -8,15 +8,21 @@ const paymentController = require("./controller/paymentController");
 const PORT=process.env.PORT;
 
 const app = express();
-app.use(cors(
-    {
-        origin: ["https://course-selling-app-six.vercel.app"],
-        methods: ["POST", "GET"],
-        credentials: true,
-        optionsSuccessStatus: 200,
-    }
-));
-app.options('*', cors());
+// app.use(cors(
+//     {
+//         origin: ["https://course-selling-app-six.vercel.app"],
+//         methods: ["POST", "GET"],
+//         credentials: true,
+//         optionsSuccessStatus: 200,
+//     }
+// ));
+// app.options('*', cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 app.use(express.json());
 // Set the view engine to EJS
